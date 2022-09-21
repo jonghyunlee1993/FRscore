9e4c8d3c94089885c83bd0cf53fcb5fe11085df9
Jonghyun Lee <green198800@hanyang.ac.kr>
2022-09-21 19:50:17 +0900

    Update threshold and add requirements.txt

--- patient_regression.py
+++ patient_regression.py
@@ -1,7 +1,6 @@
-import math
 
 def FR_score(v1, v2, AST, ALT, PLT):
-    score = 0.0153 * v1 + 0.0436 * v2 + 0.0054 * AST + \
-        - 0.0016 * ALT - 0.0024 * PLT + 1.7874
+    score = 1.53 * v1 + 4.36 * v2 + 0.54 * AST + \
+        - 0.16 * ALT - 0.24 * PLT
         
     return score
